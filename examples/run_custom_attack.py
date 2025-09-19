90fc322c9a1468bc920f25647f220b028268a24f
Iqra171 <iqrazaf05@gmail.com>
2025-09-19 21:38:18 +0500

    Create run_custom_attack.py

---
+++ examples/run_custom_attack.py
@@ -0,0 +1,19 @@
+# examples/run_custom_attack.py
+import argparse
+from pygip.datasets.datasets import Dataset
+from src.custom_attack import FeatureFlipAttack
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="Cora")
+    parser.add_argument("--fraction", type=float, default=0.1)
+    parser.add_argument("--device", type=str, default=None)
+    args = parser.parse_args()
+
+    dataset = Dataset(api_type="pyg", path="./data")
+    attack = FeatureFlipAttack(dataset, attack_node_fraction=args.fraction, model_path=None, device=args.device)
+    results = attack.attack(retrain_target=True, retrain_epochs=50, seed=0)
+    print("Attack results:", results)
+
+if __name__ == "__main__":
+    main()
